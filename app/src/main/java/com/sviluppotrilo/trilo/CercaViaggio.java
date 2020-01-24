e3c0e1b959bbbf5e138c18aac19f9e467e4056e7
luca dimeo <l.dimeo@campus.unimib.it>
2020-01-24 02:03:38 +0100

    Implementato Navigation Drawer (Menù)

--- app/src/main/java/com/sviluppotrilo/trilo/CercaViaggio.java
+++ app/src/main/java/com/sviluppotrilo/trilo/CercaViaggio.java
@@ -1,14 +1,20 @@
 package com.sviluppotrilo.trilo;
 
+
+import android.app.DatePickerDialog;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
-import android.widget.CalendarView;
+import android.widget.Button;
+import android.widget.DatePicker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.net.CacheRequest;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 public class CercaViaggio extends AppCompatActivity {
 
@@ -17,6 +23,9 @@ public class CercaViaggio extends AppCompatActivity {
     AutoCompleteTextView autoCom1;
     AutoCompleteTextView autoCom2;
     ArrayAdapter arrayAdapter;
+    Button data;
+    Calendar calendar;
+    DatePickerDialog datePickerDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,22 +34,53 @@ public class CercaViaggio extends AppCompatActivity {
         myDbHelper = new DataBaseHelper(getApplicationContext());
         myDbHelper.createDatabase();
         myData = myDbHelper.selectAllData();
-        //stazionepartenza
+
+        //Stazionepartenza
         autoCom1 = findViewById(R.id.stazionepartenza);
         arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line, myData);
         autoCom1.setAdapter(arrayAdapter);
-        //stazionearrivo
+
+        //Stazionearrivo
         autoCom2 = findViewById(R.id.stazionearrivo);
         arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line, myData);
         autoCom2.setAdapter(arrayAdapter);
-        //data
-        CalendarView data = findViewById(R.id.datapartenza);
-        long dataSelezionata = data.getDate();
-        //ora
+
+        //Data
+        data = findViewById(R.id.data);
+        String date = new SimpleDateFormat("dd-MM-yy", Locale.getDefault()).format(new Date());
+        data.setText(date);
+
+        /*
+        data.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                calendar.getInstance();
+                int giorno = calendar.get(Calendar.DAY_OF_MONTH);
+                int mese = calendar.get(Calendar.MONTH);
+                int anno = calendar.get(Calendar.YEAR);
+
+                datePickerDialog = new DatePickerDialog(CercaViaggio.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int a, int m, int g) {
+                        data.setText(g + "-" +(m+1)+ "-" + a);
+                    }
+                }, anno, mese, giorno);
+                datePickerDialog.show();
+
+            }
+        });
+
+        */
 
     }
 
-    public String getStazionePartenza(){
+    public String getStazionePartenza() {
         return String.valueOf(autoCom1.getText());
     }
+
+    public String getStazioneArrivo() {
+        return String.valueOf(autoCom2.getText());
+    }
+
+
 }
--- app/src/main/java/com/sviluppotrilo/trilo/Preferiti.java
+++ app/src/main/java/com/sviluppotrilo/trilo/Preferiti.java
@@ -2,43 +2,88 @@ package com.sviluppotrilo.trilo;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
-//Classe relativa ai activity_preferiti dell'utente
-public class Preferiti extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class Preferiti extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     Button cercaviaggio;
-    Button prova;
+    DrawerLayout drawerLayout;
+    Toolbar toolbar;
+    NavigationView navigationView;
+    ActionBarDrawerToggle toggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preferiti);
+        toolbar = findViewById(R.id.toolbar);
+        navigationView = findViewById(R.id.navigation);
+        drawerLayout = findViewById(R.id.drawer);
 
         cercaviaggio = findViewById(R.id.cercabt);
         cercaviaggio.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                //Al click sul bottone viene aperta la pagina dei ricerca viaggio
                 Intent goToCercaViaggio = new Intent(Preferiti.this, CercaViaggio.class);
                 Preferiti.this.startActivity(goToCercaViaggio);
             }
         });
 
-        //bottone prova
-        prova = findViewById(R.id.prova);
-        prova.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent goToMainActivity = new Intent(Preferiti.this, MainActivity.class);
-                Preferiti.this.startActivity(goToMainActivity);
-                Log.d("SONO IN prefe ultim", "MSG");
-            }
-        });
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.drawerOpen, R.string.drawerClose);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
 
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.gestiscipreferiti:
+                Intent goToPreferiti = new Intent(Preferiti.this, Preferiti.class);
+                startActivity(goToPreferiti);
+                break;
+            case R.id.gestiscinotifiche:
+                //Intent goToNotifiche = new Intent(Preferiti.this, Preferiti.class);
+                //startActivity(goToNotifiche);
+                break;
+            case R.id.impostazioni:
+                Intent goToImpostazioni = new Intent(Preferiti.this, Impostazioni.class);
+                startActivity(goToImpostazioni);
+                break;
+            case R.id.socialnetwork:
+                //Intent goToSocial = new Intent(Preferiti.this, Preferiti.class);
+                //startActivity(goToSocial);
+                break;
+            case R.id.profilo:
+                Intent goToProfilo = new Intent(Preferiti.this, Profilo.class);
+                startActivity(goToProfilo);
+                break;
+            case R.id.mappe:
+                //Intent goToMappe = new Intent(Preferiti.this, Mappa.class);
+                //startActivity(goToMappe);
+                break;
+            case R.id.aboutus:
+                //Intent goToAboutUs = new Intent(Preferiti.this, Preferiti.class);
+                //startActivity(goToAboutUs);
+                break;
+            default:
+                break;
+        }
+        return false;
     }
 
 }
