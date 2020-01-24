5106a05900fcb962f1b2b95f03103c985624b265
Marco Poveromo <m.poveromo@campus.unimib.it>
2020-01-24 11:14:04 +0100

    - Aggiunta Observer Notifica

--- app/src/main/java/com/sviluppotrilo/trilo/Notifica.java
+++ app/src/main/java/com/sviluppotrilo/trilo/Notifica.java
@@ -4,13 +4,20 @@ import java.util.Observable;
 import java.util.Observer;
 
 public class Notifica implements Observer {
+    //Qui dovrei controllare se
+    // 1) corsa.tipoTreno.equals("PG") && provvedimento == 0 allora è regolare
+    // 2) corsa.tipoTreno.euqals("ST") e provvedimento==1: treno soppresso
+    // 3) corsa.tipoTreno.equals("PP") oppure 'SI' oppure 'SF' e provvedimento vale 0 oppure 2:
+    //     allora il treno è parzialmente soppresso
+    // Quindi se sono nello stato "regolare" e durante l'update cambia stato allora invia
+    // la notifica
 
     public Corsa corsa;
 
     @Override
     public void update(Observable o, Object corsa) {
         this.setCorsa((Corsa) corsa);
-        System.out.println("La notifica ora controlla lo stato di corsa");
+        //Qui controllo se devo cambiare stato e nel caso lo cambio
     }
 
     public Corsa getCorsa() {
