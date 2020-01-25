93dccdf348ec3e9317faf41f2abc0a41399213b9
Marco Poveromo <m.poveromo@campus.unimib.it>
2020-01-25 19:10:20 +0100

    - Aggiunta la possibilità di salvare permanentemente i giorni

--- app/src/main/java/com/sviluppotrilo/trilo/SplashActivity.java
+++ app/src/main/java/com/sviluppotrilo/trilo/SplashActivity.java
@@ -1,10 +1,14 @@
 package com.sviluppotrilo.trilo;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
+import android.preference.PreferenceManager;
 import android.widget.ProgressBar;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.preference.PowerPreference;
 
 
 public class SplashActivity extends AppCompatActivity {
@@ -20,6 +24,9 @@ public class SplashActivity extends AppCompatActivity {
         setContentView(R.layout.splash_theme); //Viene settato il Layout
         mProgressBar = findViewById(R.id.progressBar); //Viene referenziata la ProgressBar presente nel layout
 
+        //Inizializzazione dei preferiti
+        initPreferiti();
+        
         // Creo un thread per l'avanzamento della ProgressBar
         new Thread(new Runnable() {
             @Override
@@ -44,5 +51,33 @@ public class SplashActivity extends AppCompatActivity {
                 });
             }
         }).start();
+    }
+
+    private void initPreferiti(){
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        if(!prefs.getBoolean("firstTime", false)) {
+            firstRun();
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putBoolean("firstTime", true);
+            editor.commit();
+        }
+    }
+
+    private void firstRun(){
+        PowerPreference.init(this);
+        Giorno lunedi = new Giorno(1, "Lunedi");
+        boolean result = PowerPreference.getDefaultFile().setObject("Lunedi", lunedi);
+        Giorno martedi = new Giorno(2, "Martedi");
+        result = PowerPreference.getDefaultFile().setObject("Martedi", martedi);
+        Giorno mercoledi = new Giorno(3, "Mercoledi");
+        result = PowerPreference.getDefaultFile().setObject("Mercoledi", mercoledi);
+        Giorno giovedi = new Giorno(4, "Giovedi");
+        result = PowerPreference.getDefaultFile().setObject("Giovedi", giovedi);
+        Giorno venerdi = new Giorno(5, "Venerdi");
+        result = PowerPreference.getDefaultFile().setObject("Venerdi", venerdi);
+        Giorno sabato = new Giorno(6, "Sabato");
+        result = PowerPreference.getDefaultFile().setObject("Sabato", sabato);
+        Giorno domenica = new Giorno(7, "Domenica");
+        result = PowerPreference.getDefaultFile().setObject("Domenica", domenica);
     }
 }
