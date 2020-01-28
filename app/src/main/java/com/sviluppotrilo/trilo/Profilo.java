455ce24e54fd8d1a85d9996643b53463e741d39f
Simone Fermini <s.fermini@campus.unimib.it>
2020-01-28 13:52:48 +0100

    Salvataggio automatico valori del profilo

--- app/src/main/java/com/sviluppotrilo/trilo/Profilo.java
+++ app/src/main/java/com/sviluppotrilo/trilo/Profilo.java
@@ -1,14 +1,43 @@
 package com.sviluppotrilo.trilo;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.preference.PowerPreference;
+
 public class Profilo extends AppCompatActivity {
+
+    EditText nome;
+    EditText cognome;
+    EditText dataN;
+    Button salva;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profilo);
+
+        nome = findViewById(R.id.nome);
+        cognome = findViewById(R.id.cognome);
+        dataN = findViewById(R.id.datadinascita);
+        salva = findViewById(R.id.salvadatiprofilo);
+
+        nome.setText(PowerPreference.getDefaultFile().getString("nome",""));
+        cognome.setText(PowerPreference.getDefaultFile().getString("cognome",""));
+
+        salva.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PowerPreference.getDefaultFile()
+                        .putString("nome", nome.getText().toString())
+                        .putString("cognome", cognome.getText().toString());
+            }
+        });
+
+
     }
 }
