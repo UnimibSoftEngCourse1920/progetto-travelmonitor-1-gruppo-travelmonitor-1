62958a52ef86fe83ba82a7e89e1953dadad002a6
Simone Fermini <s.fermini@campus.unimib.it>
2020-01-30 17:22:56 +0100

    Aggiunta salvataggio stato impostazioni notifiche

--- app/src/main/java/com/sviluppotrilo/trilo/gui/Impostazioni.java
+++ app/src/main/java/com/sviluppotrilo/trilo/gui/Impostazioni.java
@@ -1,17 +1,27 @@
 package com.sviluppotrilo.trilo.gui;
 
 import android.content.Intent;
+import android.content.pm.LauncherApps;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
+import android.widget.Switch;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import com.preference.PowerPreference;
 import com.sviluppotrilo.trilo.R;
 
 public class Impostazioni extends AppCompatActivity {
 
     ImageView back;
+    Switch notificaRitardo;
+    Switch notificaCancellazione;
+    Switch notificaFuoriCitta;
+    Switch notificaPromemoria;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +32,58 @@ public class Impostazioni extends AppCompatActivity {
         findViewById(R.id.textView3).setSelected(true);
         findViewById(R.id.textView6).setSelected(true);
         findViewById(R.id.textView8).setSelected(true);
+
+
+        notificaRitardo = findViewById(R.id.switch1);
+        notificaCancellazione = findViewById(R.id.switch2);
+        notificaFuoriCitta = findViewById(R.id.switch3);
+        notificaPromemoria = findViewById(R.id.switch4);
+
+        notificaRitardo.setChecked(PowerPreference.getFileByName("impostazioni").getBoolean("notificaRitardo", false));
+        notificaCancellazione.setChecked(PowerPreference.getFileByName("impostazioni").getBoolean("notificaCancellazione", false));
+        notificaFuoriCitta.setChecked(PowerPreference.getFileByName("impostazioni").getBoolean("notificaFuoriCitta", false));
+        notificaPromemoria.setChecked(PowerPreference.getFileByName("impostazioni").getBoolean("notificaPromemoria", false));
+
+        notificaRitardo.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                if(isChecked) {
+                    PowerPreference.getFileByName("impostazioni").setBoolean("notificaRitardo", true);
+                }else {
+                    PowerPreference.getFileByName("impostazioni").setBoolean("notificaRitardo", false);
+                }
+            }
+        });
+
+        notificaCancellazione.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                if(isChecked)
+                    PowerPreference.getFileByName("impostazioni").setBoolean("notificaCancellazione",true);
+                else
+                    PowerPreference.getFileByName("impostazioni").setBoolean("notificaCancellazione",false);
+            }
+        });
+
+        notificaFuoriCitta.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                if(isChecked)
+                    PowerPreference.getFileByName("impostazioni").setBoolean("notificaFuoriCitta",true);
+                else
+                    PowerPreference.getFileByName("impostazioni").setBoolean("notificaFuoriCitta",false);
+            }
+        });
+
+        notificaPromemoria.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                if(isChecked)
+                    PowerPreference.getFileByName("impostazioni").setBoolean("notificaPromemoria",true);
+                else
+                    PowerPreference.getFileByName("impostazioni").setBoolean("notificaPromemoria",false);
+            }
+        });
 
         back = findViewById(R.id.backpage);
 
